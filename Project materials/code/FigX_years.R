54d79738861a48eef4d706a3899d1547d9e108e8
Kate <katherine.hayes@ucdenver.edu>
2022-01-07 11:02:09 -0700

    changing bin width to 5 / centering bars so they start at 0

--- Project materials/code/FigX_years.R
+++ Project materials/code/FigX_years.R
@@ -9,7 +9,7 @@ data <- read.csv(here("data/Grad_survey.csv"))
 yr_fieldplot <- data %>%
   filter(Career_Stage != "NA") %>%
   filter(Career_Stage != "") %>%
-  ggplot(aes(x = Yr_field, fill = Career_Stage)) + geom_histogram() + 
+  ggplot(aes(x = Yr_field, fill = Career_Stage)) + geom_histogram(binwidth = 5, center = 2.5) + 
   labs(title = "Field experience",
        x = "Years") + theme(legend.position = "none")
 yr_fieldplot
@@ -17,7 +17,7 @@ yr_fieldplot
 yr_leadplot <- data %>%
   filter(Career_Stage != "NA") %>%
   filter(Career_Stage != "") %>%
-  ggplot(aes(x = Yr_lead, fill = Career_Stage)) + geom_histogram() + 
+  ggplot(aes(x = Yr_lead, fill = Career_Stage)) + geom_histogram(binwidth = 5, center = 2.5) + 
   labs(title = "Experience leading fieldwork",
        x = "Years") + theme(legend.position = "none")
 yr_leadplot
@@ -25,10 +25,29 @@ yr_leadplot
 yr_supplot <- data %>%
   filter(Career_Stage != "NA") %>%
   filter(Career_Stage != "") %>%
-  ggplot(aes(x = Yr_super, fill = Career_Stage)) + geom_histogram() + 
+  ggplot(aes(x = Yr_super, fill = Career_Stage)) + geom_histogram(binwidth = 5, center = 2.5) + 
   labs(title = "Experience supervising fieldwork",
        x = "Years")
 yr_supplot
 
 plot_grid(yr_fieldplot, yr_leadplot, yr_supplot, nrow = 1, ncol = 3,
           rel_widths = c(0.75, 0.75, 1.25), labels = c("A.", "B.", "C.")) # width 1350, height 350
+
+
+# summary stats
+# doing fieldwork
+length(which(data$Yr_field > 5)) # n
+
+length(which(data$Yr_field > 5)) / 94 * 100 # %
+
+
+# leading
+length(which(data$Yr_lead > 5))
+
+length(which(data$Yr_lead > 5)) / 94 * 100
+
+# supervising
+length(which(data$Yr_super > 5))
+
+length(which(data$Yr_super> 5)) / 94 * 100
+
